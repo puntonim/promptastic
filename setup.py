bff7e4f56869a98c95e351c982a5efef30316ee0
nimiq <puntonim@gmail.com>
2014-10-01 23:56:09 +0200

    Setup procedure complete

--- setup.py
+++ setup.py
@@ -1,19 +1,92 @@
-# In order to install this new prompt we have to add a few lines at those files source every time
-# a Bash shell is invokes.
-# Two are the files we need to edit, as explained here:
-# http://mywiki.wooledge.org/DotFiles
-# Every time a Bash shell is invoked, the file `~/.bash_profile` is read, if it exists.
-# While the file `~/.bashrc` is read when a subshell is invoked with a command like `bash`.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+In order to install promptastic a few lines have to be appended to those files sourced every time
+a Bash shell is invoked.
+The affected files (as explained at http://mywiki.wooledge.org/DotFiles) are:
+~/.bash_profile - read every time a Bash shell is invoked,
+~/.bashrc - read when a subshell is invoked with a command like `bash`.
+"""
+import re
+import os
 
 
-# Read the files ~/.bash_profile and ~/.bashrc.
-# Check if the text `function _update_ps1() { .* }` is included:
-# if not: add it; else: if it is different from our text, comment it out and add it.
-#function _update_ps1() {
-#   export PS1="$(~/workspace/promptastic/promptastic.py $?)"
-#}
-#
-# Check if the text `export PROMPT_COMMAND` is included:
-# if not: add it; else: if it is different from our text, comment it out and add it.
-#export PROMPT_COMMAND="_update_ps1"
+FUNCTION_CMD = 'function _update_ps1() {{ export PS1="$({}/promptastic.py $?)"; }}'
+PROMPT_CMD = 'export PROMPT_COMMAND="_update_ps1"'
 
+
+class ConfigFile:
+    def __init__(self):
+        self.is_already_setup = self._is_already_setup()
+
+    @staticmethod
+    def _get_cwd(expanded=True):
+        if expanded:
+            return os.getcwd()
+        return os.getcwd().replace(os.path.expanduser('~'), '~')
+
+    def _is_already_setup(self):
+        try:
+            with open(os.path.expanduser(self.path)) as file:
+                content = file.read()
+        except FileNotFoundError:
+            return False
+
+        # Check for `PROMPT_CMD`:
+        match = re.search(r'^[ \t]*{}[ \t;]*$'.format(PROMPT_CMD), content, re.M)
+        if not match:
+            # So the file does not contain `PROMPT_CMD`.
+            return False
+
+        # So the file does contain `PROMPT_CMD`.
+        # Check for `FUNCTION_CMD`:
+        regex = r'^[ \t]*{}|{}[ \t;]*$'.format(
+            re.escape(FUNCTION_CMD.format(self._get_cwd(True))),
+            re.escape(FUNCTION_CMD.format(self._get_cwd(False))),
+        )
+        match = re.search(regex, content, re.M)
+        if not match:
+            # So the file does not contain `FUNCTION_CMD`.
+            return False
+
+        # So the file does not contain `FUNCTION_CMD`.
+        print('Promptastic already installed in {}'.format(self.path))
+        return True
+
+    def install(self):
+        print('\nInstalling to {}...'.format(self.path), end=' ')
+        with open(os.path.expanduser(self.path), 'a') as file:
+            file.write('\r\n\r\n' + FUNCTION_CMD.format(self._get_cwd()) + '\r\n')
+            file.write(PROMPT_CMD + '\r\n')
+        print('done')
+
+
+class BashProfile(ConfigFile):
+    path = '~/.bash_profile'
+
+
+class BashRc(ConfigFile):
+    path = '~/.bashrc'
+
+
+if __name__ == '__main__':
+    print('\nPromptastic setup\n=================')
+    print('\nChecking if promptastic is already installed in the system...')
+
+    bash_profile = BashProfile()
+    bash_rc = BashRc()
+
+    to_be_installed = [x for x in (bash_profile, bash_rc) if not x.is_already_setup]
+
+    if to_be_installed:
+        text = ' and '.join([x.path for x in to_be_installed])
+        print('\nWe are about to install promptastic in {}'.format(text))
+        a = input('Are you sure you want to continue [y/N]? ')
+
+        if a == 'y':
+            for x in to_be_installed:
+                x.install()
+
+    print('\nSetup complete!\n')
+    exit(0)
