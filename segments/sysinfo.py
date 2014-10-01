140e904b36655a3254fa2523f9fa363d023d266b
nimiq <puntonim@gmail.com>
2014-10-01 15:46:11 +0200

    Alternative way to get the number of jobs

--- segments/sysinfo.py
+++ segments/sysinfo.py
@@ -25,6 +25,42 @@ class Jobs(Segment):
         if not num_jobs:
             self.active = False
 
+    # The following code is an alternative way to get the number of jobs by reading `/proc` folder.
+    # It works only on linux (no Mac OS) and it has better performance making it especially
+    # suitable for old machines.
+    # Source: https://github.com/milkbikis/powerline-shell/issues/117
+    #
+    # myppid = os.getppid()
+    # count_jobs = {}
+    # process_pid = -1
+    #
+    # for f in os.listdir('/proc'):
+    #     pathname = os.path.join('/proc', f)
+    #     fstat = os.stat(pathname)
+    #
+    #     if fstat.st_uid != os.getuid():
+    #         continue
+    #
+    #     if S_ISDIR(fstat.st_mode):
+    #         statfile = os.path.join(pathname, 'stat')
+    #         if os.path.isfile(statfile):
+    #             with open(statfile) as f:
+    #                 statline = f.readline()
+    #                 fields   = statline.split()
+    #                 if len(fields) >= 3:
+    #                     process_pid  = fields[0]
+    #                     process_ppid = fields[3]
+    #
+    #                     if process_pid == str(myppid):
+    #                        process_pid = process_ppid
+    #
+    #                     if count_jobs.has_key(process_ppid):
+    #                        count_jobs[process_ppid] += 1
+    #                     else:
+    #                        count_jobs[process_ppid]  = 1
+    #
+    # num_jobs = count_jobs[str(process_pid)] - 1
+
 
 class Time(Segment):
     bg = colors.background(colors.DARKER_GREY)
